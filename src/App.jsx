6c97867b176bc0aed16b55b969f522a56382c003
mariomtzdev <99843020+devmariomtz@users.noreply.github.com>
2024-06-17 18:53:13 -0600

    📦️ Add Services component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from "react";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Form from "./components/Form";
 import Trayectoria from "./components/Trayectoria";
+import Services from "./components/Services";
 
 function App() {
   return (
-    <React.Fragment>
-      <div className="text-white bg-fixed bg-center bg-cover bg-no-repeat bg-hero">
-        <NavBar />
-        <Hero />
-        <Form />
-        <Trayectoria />
-      </div>
-    </React.Fragment>
+    <div className="text-white bg-fixed bg-center bg-cover bg-no-repeat bg-hero">
+      <NavBar />
+      <Hero />
+      <Form />
+      <Trayectoria />
+      <Services />
+    </div>
   );
 }
 
---
+++ src/components/Services.jsx
@@ -0,0 +1,74 @@
+const Services = () => {
+  return (
+    <div className="bg-[#000b17] pt-4">
+      <h2 className="text-4xl md:text-6xl w-[50%] text-end">
+        Nuestros <span className="font-bold">servicios</span>
+      </h2>
+      <hr className="bg-[#2d91d4] w-[50%] h-2 border-t-0" />
+      <div className="flex flex-wrap gap-5 md:gap-16 justify-center p-6">
+        <div className="md:w-[27vw] md:h-[25vw] w-full h-auto bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 flex flex-col items-center justify-center text-center">
+          <img src="src/assets/estrategia.png" alt="" className="w-[35%]" />
+          <div className="text-xl p-4">
+            <h3 className="font-bold">ESTRATEGIA VALIDADA</h3>
+            <p className="font-light">
+              Te guiaremos a través de una estrategia probada y validada.
+            </p>
+          </div>
+        </div>
+        <div className="md:w-[27vw] md:h-[25vw] w-full h-auto bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 flex flex-col items-center justify-center text-center">
+          <img
+            src="src/assets/material-exclusivo.png"
+            alt=""
+            className="w-[35%]"
+          />
+          <div className="text-xl p-4">
+            <h3 className="font-bold">MATERIAL EXCLUSIVO</h3>
+            <p className="font-light">
+              Lectura y materiales exclusivos por nuestro equipo.
+            </p>
+          </div>
+        </div>
+        <div className="md:w-[27vw] md:h-[25vw] w-full h-auto bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 flex flex-col items-center justify-center text-center">
+          <img src="src/assets/mentoria.png" alt="" className="w-[35%]" />
+          <div className="text-xl p-4">
+            <h3 className="font-bold">MENTORÍA PERSONALIZADA</h3>
+            <p className="font-light">
+              Recibirás mentorías con preguntas y orientación personalizada.
+            </p>
+          </div>
+        </div>
+        <div className="md:w-[27vw] md:h-[25vw] w-full h-auto bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 flex flex-col items-center justify-center text-center">
+          <img src="src/assets/comunidad.png" alt="" className="w-[35%]" />
+          <div className="text-xl p-4">
+            <h3 className="font-bold">COMUNIDAD ACTIVA</h3>
+            <p className="font-light">
+              Comunidad apasionada de estudiantes y traders.
+            </p>
+          </div>
+        </div>
+        <div className="md:w-[27vw] md:h-[25vw] w-full h-auto bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 flex flex-col items-center justify-center text-center">
+          <img
+            src="src/assets/material-exclusivo.png"
+            alt=""
+            className="w-[35%]"
+          />
+          <div className="text-xl p-4">
+            <h3 className="font-bold">ESTRATEGIAS EXCLUSIVAS</h3>
+            <p className="font-light">
+              Diseñado para igualar nuestra propia altura de conocimiento.
+            </p>
+          </div>
+        </div>
+        <div className="md:w-[27vw] md:h-[25vw] w-full h-auto bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 flex flex-col items-center justify-center text-center">
+          <img src="src/assets/online.png" alt="" className="w-[35%]" />
+          <div className="text-xl p-4">
+            <h3 className="font-bold">CURSO DESDE CERO</h3>
+            <p className="font-light">Aprende los fundamentos del trading.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
